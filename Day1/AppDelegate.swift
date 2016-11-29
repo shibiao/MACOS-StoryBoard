993158577a454ea0f170865540680c190a8f9a2c
WalkWithWind <sycf_ios_13579@sycf-iosdeiMac.local>
2016-11-29 17:23:06 +0800

    Initial Commit

---
+++ Day1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Day1
+//
+//  Created by sycf_ios on 2016/11/29.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Day1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Day1
+//
+//  Created by sycf_ios on 2016/11/29.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
